0cdd21bf8b99529b599d8f732edbedc6df513139
sergio <sergio.joselli@gmail.com>
2026-01-03 16:43:11 -0300

    feat: add logging to entrypoint layer

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-from src.infrastructure.driven.config._impl import CONFIG_ADAPTER
 from src.infrastructure.driven.candle_source.csv import CSVCandleSourceAdapter
 from src.infrastructure.driven.journal.csv import CSVJournalAdapter
 
@@ -10,28 +9,40 @@ from src.infrastructure.driven.logger import logger
 
 def main():
     logger.info("--- Starting Simulation ---")
+    try:
+        logger.info("[Init] Initialing system...")
+        logger.info(f"[Init] Loading config...")
+        from src.infrastructure.driven.config._impl import CONFIG_ADAPTER
+        logger.info(f"[Init] Config loaded.")
 
-    logger.info(f"[Init] Configuration loaded.")
+        logger.info(f"[Init] Loading candle source...")
+        candle_source = CSVCandleSourceAdapter(file_name="input.csv")
+        logger.info("[Init] CSV Candle Source ready.")
 
-    candle_source = CSVCandleSourceAdapter(file_name="input.csv")
-    logger.info("[Init] CSV Candle Source ready.")
+        logger.info(f"[Init] Loading journal...")
+        journal = CSVJournalAdapter(file_name="output.csv")
+        logger.info("[Init] CSV Journal ready.")
 
-    journal = CSVJournalAdapter(file_name="output.csv")
-    logger.info("[Init] CSV Journal ready.")
-
-    if CONFIG_ADAPTER.random_strategy:
-        strategy = RandomStrategy(config=CONFIG_ADAPTER)
-        logger.info(f"[Init] 'RANDOM_STRATEGY' is True. Overriding with '{type(strategy).__name__}'.")
-    else:
-        strategy = MeanReversionStrategy(config=CONFIG_ADAPTER)
+        logger.info("[Init] Loading strategy engine...")
+        if CONFIG_ADAPTER.random_strategy:
+            strategy = RandomStrategy(config=CONFIG_ADAPTER)
+            logger.info(f"[Init] 'random_strategy' is True. Overriding with '{type(strategy).__name__}'.")
+        else:
+            strategy = MeanReversionStrategy(config=CONFIG_ADAPTER)
         logger.info(f"[Init] Strategy '{type(strategy).__name__}' initialized.")
 
-    engine = SimulatedTradingEngine(
-        strategy=strategy,
-        source=candle_source,
-        journal=journal
-    )
-    logger.info("[Init] Simulated Trading Engine assembled.")
+        logger.info(f"[Ini] Loading trading engine...")
+        engine = SimulatedTradingEngine(
+            strategy=strategy,
+            source=candle_source,
+            journal=journal
+        )
+        logger.info("[Init] Simulated Trading Engine assembled.")
+        logger.info("[Init] Initialization finalized successfully.")
+    except Exception as e:
+        logger.error(f"[Init Fatal] Failed to initialize application: {e}")
+        # Re-raise to ensure the process actually exits with an error code.
+        raise e
 
     logger.info("--- Execution Started ---")
     try:
@@ -39,7 +50,7 @@ def main():
         logger.info("--- Execution Finished Successfully ---")
         logger.info("Results written to 'output.csv'.")
     except Exception as e:
-        logger.error(f"\n[Error] Execution crashed: {e}.")
+        logger.error(f"\n[Runtime Fatal] Execution crashed: {e}.")
         raise e
 
 if __name__ == "__main__":
